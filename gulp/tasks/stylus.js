25944f4828e62a0bc325b232a19d50757affd224
brenopolanski <breno.polanski@gmail.com>
2016-08-26 23:06:17 -0300

    SKU-1524 #comment Update CSS Stylus

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -4,24 +4,24 @@
 var gulp         = require('gulp');
 var plumber      = require('gulp-plumber');
 var stylus       = require('gulp-stylus');
-var poststylus   = require('poststylus');
+var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var mqpacker     = require('css-mqpacker');
+var csswring     = require('csswring');
 var csscomb      = require('gulp-csscomb');
 var paths        = require('../paths');
 
 // Call Stylus
 module.exports = gulp.task('stylus', function() {
   var processors = [
-    autoprefixer
+    autoprefixer({ browsers: ['last 3 versions'] }),
+    mqpacker,
+    csswring
   ];
   return gulp.src(paths.source.files.styl)
     .pipe(plumber())
-    .pipe(stylus({
-      compress: false,
-      use: [
-        poststylus(processors)
-      ]
-    }))
+    .pipe(stylus())
     .pipe(csscomb())
+    .pipe(postcss(processors))
     .pipe(gulp.dest(paths.build.css));
 });
