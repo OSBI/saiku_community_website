24c228c80b453689d5c9ea5edd395c26c3def485
brenopolanski <breno.polanski@gmail.com>
2016-08-28 18:16:02 -0300

    SKU-1524 #comment Add templates jade

--- gulp/tasks/jade.js
+++ gulp/tasks/jade.js
@@ -4,6 +4,7 @@
 var gulp    = require('gulp');
 var plumber = require('gulp-plumber');
 var jade    = require('gulp-jade');
+var data    = require('../../src/data/data');
 var paths   = require('../paths');
 
 // Call Jade to compile templates
@@ -11,7 +12,10 @@ module.exports = gulp.task('jade', function() {
   return gulp.src(paths.source.files.jade)
     .pipe(plumber())
     .pipe(jade({
-      pretty: true
+      pretty: true,
+      locals: {
+        data: data
+      }
     }))
     .pipe(gulp.dest(paths.build.html));
 });
---
+++ src/data/data.js
@@ -0,0 +1,137 @@
+/**
+ *   Copyright 2016 OSBI Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+module.exports = {
+  social: [
+    {
+      name: 'Twitter',
+      icon: 'twitter',
+      url: 'https://twitter.com/SaikuAnalytics'
+    },
+    {
+      name: 'Facebook',
+      icon: 'facebook',
+      url: 'https://www.facebook.com/meteoritebi'
+    },
+    {
+      name: 'LinkedIn',
+      icon: 'linkedin',
+      url: 'https://www.linkedin.com/company/meteorite-consulting'
+    },
+    {
+      name: 'GitHub',
+      icon: 'github-alt',
+      url: 'https://github.com/OSBI'
+    }
+  ],
+  main: [
+    {
+      title: 'Download Saiku CE',
+      subtitle: 'For Windows/Mac/Linux',
+      icon: 'download',
+      url: '#',
+      id: 'trigger-overlay'
+    },
+    {
+      title: 'Source Code',
+      subtitle: 'github.com/osbi',
+      icon: 'github',
+      url: 'https://github.com/osbi',
+      id: null
+    },
+    {
+      title: 'Docs REST API',
+      subtitle: 'community.meteorite.bi/docs/',
+      icon: 'file-code-o',
+      url: 'http://community.meteorite.bi/docs/',
+      id: null
+    },
+    {
+      title: 'Saiku Dev Group',
+      subtitle: 'groups.google.com',
+      icon: 'bolt',
+      url: 'https://groups.google.com/a/saiku.meteorite.bi/forum/#!forum/dev',
+      id: null
+    },
+    {
+      title: 'Saiku User Group',
+      subtitle: 'groups.google.com',
+      icon: 'users',
+      url: 'https://groups.google.com/a/saiku.meteorite.bi/forum/#!forum/user',
+      id: null
+    },
+    {
+      title: 'Wiki',
+      subtitle: 'wiki.meteorite.bi',
+      icon: 'text-o',
+      url: 'http://wiki.meteorite.bi/display/SAIK/Saiku',
+      id: null
+    },
+    {
+      title: 'JIRA',
+      subtitle: 'jira.meteorite.bi',
+      icon: 'bug',
+      url: 'http://jira.meteorite.bi/secure/Dashboard.jspa',
+      id: null
+    },
+    {
+      title: 'Stack Overflow',
+      subtitle: 'stackoverflow.com/saiku',
+      icon: 'stack-overflow',
+      url: 'http://stackoverflow.com/questions/tagged/saiku',
+      id: null
+    },
+    {
+      title: 'Freenode IRC',
+      subtitle: 'Channel: #saiku',
+      icon: 'comments',
+      url: 'http://webchat.freenode.net/?randomnick=1&channels=%23%23saiku',
+      id: null
+    }
+  ],
+  footer: {
+    github: [
+      {
+        name: 'Breno Polanski',
+        nameRaw: 'Breno<br>Polanski',
+        url: 'https://github.com/brenopolanski',
+        image: 'https://avatars1.githubusercontent.com/u/1894191?v=3&s=50'
+      },
+      {
+        name: 'Luis E. Garcia',
+        nameRaw: 'Luis E.<br>Garcia',
+        url: 'https://github.com/PeterFalken',
+        image: 'https://avatars2.githubusercontent.com/u/2557898?v=3&s=50'
+      },
+      {
+        name: 'Tom Barber',
+        nameRaw: 'Tom<br>Barber',
+        url: 'https://github.com/buggtb',
+        image: 'https://avatars1.githubusercontent.com/u/103544?v=3&s=50'
+      },
+      {
+        name: 'Paul Stoellberger',
+        nameRaw: 'Paul<br>Stoellberger',
+        url: 'https://github.com/pstoellberger',
+        image: 'https://avatars1.githubusercontent.com/u/454645?v=3&s=50'
+      }
+    ],
+    company: {
+      name: '',
+      url: ''
+    }
+  }
+};
