00cb8d88b37b8658ad33e286435aafd365dcf860
brenopolanski <breno.polanski@gmail.com>
2016-08-28 14:05:10 -0300

    SKU-1524 #comment Update project

--- gulp/paths.js
+++ gulp/paths.js
@@ -6,7 +6,7 @@ module.exports = {
     slides: './src/slides/*.md',
     js: './src/js/**/*.js',
     styl: './src/styl/**/*.styl',
-    img: './src/img/**/*',
+    img: './src/images/**/*',
     files: {
       jade: './src/templates/index.jade',
       styl: './src/styl/main.styl'
@@ -17,17 +17,13 @@ module.exports = {
     html: './build/**/*.html',
     css: './build/css/**/*.css',
     js: './build/js/**/*.js',
-    img: './build/img/**/*'
+    img: './build/images/**/*'
   },
 
   build: {
     html: './build/',
     css: './build/css',
     js: './build/js',
-    img: './build/img',
-  },
-
-  deploy: {
-    pages: './build/**/*'
+    img: './build/images'
   }
 };
